774ebf9d353f73f44d1ca3cdcd90b56e1336bd7a
manya <manya@DESKTOP-LD7310J>
2022-07-01 02:28:40 +0300

    Proje dosyası ekle.

---
+++ ChartManager.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    internal class ChartManager
+    {
+        public void Ekle(Product product)
+        {
+            Console.WriteLine("Sepete Eklendi" + product.Adi);
+
+        }
+
+        public void Ekle2(string urunAdi, string Aciklama, double fiyat, int stokAdedi) 
+        { 
+
+            Console.WriteLine("Sepete Eklendi" + urunAdi ) ;
+
+        }
+
+
+       
+
+        
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿namespace ConsoleApp3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            // String urunadi = "Lenovo";
+            // double urunfiyati = 3655;
+            // string urunaciklamasi = "Lenovo Bilgisayar"; 
+
+            Product Lenovo = new Product();
+            Lenovo.Adi = "Lenovo Bilgisayar";
+            Lenovo.Fiyat = 1500;
+            Lenovo.Aciklama = "Lenovo Bilgisayar";
+
+            Product HpComputer = new Product();
+            HpComputer.Adi = "Hp 250 G4";
+            HpComputer.Fiyat = 2500;
+            HpComputer.Aciklama = "HP Gaming Computer ";
+
+            Product[] computers = new Product[] { Lenovo, HpComputer };
+            
+            foreach (Product computer in computers)
+            {
+                Console.WriteLine(computer.Adi); 
+                Console.WriteLine(computer.Fiyat);
+                Console.WriteLine(computer.Aciklama);   
+
+                
+
+            }
+
+            Console.WriteLine("---------Methods---------"); 
+            ChartManager chartManager = new ChartManager();
+            chartManager.Ekle(Lenovo);
+            chartManager.Ekle(HpComputer);
+
+            chartManager.Ekle2("Monster Notebook", "Monster Oyuncu Bilgisayari", 12599 , 65);
+            chartManager.Ekle2("Legion Laprop", "Oyuncu Bikgisayarı", 1399, 54); 
+
+            
+
+
+
+
+        }
+    }
+}
